2aa8329716feb6bb7b3887c89a06a52fefb9c986
Simon McManus <mcmanus.simon@gmail.com>
2013-11-05 14:14:00 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,51 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , bunyan = require('bunyan')
+  , CONFIG = require('config');
+
+var log = bunyan.createLogger({name: 'skeleton-app'});
+var app = express();
+
+
+app.configure('development', function(){
+  app.use(require('express-chrome-logger'));
+  app.use(express.errorHandler());
+});
+
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/', routes.index);
+
+app.get('/logging', function(req, res) {
+  console.log(CONFIG.domain)
+  res.console.log('Send to chrome logger', CONFIG); // Wil only work in development.
+  log.info('hi');
+  log.warn('Just a warning.');
+  log.error('Something went terribly wrong. :(');
+  res.send('A response.')
+})
+
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  log.info('Express server listening on port ' + app.get('port'));
+});
---
+++ config/production.js
@@ -0,0 +1,12 @@
+module.exports = {
+	domain: 'skeleton.com',
+	api: 'api.skeleton.com',
+	redis: {
+		username: 'prod-redis-username',
+		password: 'prod-redis-password'
+	},
+	database: {
+		username: 'prod-mysql-username',
+		password: 'prod-mysql-password',
+	}
+}
